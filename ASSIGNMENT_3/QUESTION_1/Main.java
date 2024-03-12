677de6448e6a7766e541ba5b84a71907908f8e24
Debadarshi Omkar <129953571+debadarshi07@users.noreply.github.com>
2024-03-12 22:21:37 +0530

    Add files via upload

---
+++ ASSIGNMENT_3/QUESTION_1/Main.java
@@ -0,0 +1,21 @@
+package Q01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Pair<Integer,String> p1=new Pair<>(18,"Virat");
+		Pair<Integer,String> p2=new Pair<>(45,"Rohit");
+		Pair<Integer,String> p3=new Pair<>(77,"Shubhman");
+		Pair<Integer,String> p4=new Pair<>(64,"Yashasvi");
+		Pair<Integer,String> p5=new Pair<>(99,"Ravichandran");
+		
+		System.out.println(p1);	
+		System.out.println(p2);	
+		System.out.println(p3);	
+		System.out.println(p4);	
+		System.out.println(p5);
+	}
+
+}
