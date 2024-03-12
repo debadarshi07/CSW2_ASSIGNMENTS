d3644c59b43c09e695975f7f4268a9e16c5b1e07
Debadarshi Omkar <129953571+debadarshi07@users.noreply.github.com>
2024-03-12 20:49:30 +0530

    Update Enrollment.java

--- ASSIGNMENT_1/QUESTION_9/Enrollment.java
+++ ASSIGNMENT_1/QUESTION_9/Enrollment.java
@@ -1,5 +1,51 @@
 package Q9;
 
-public class Enrollment {
+class Enrollment implements EnrollmentSystem {
+    private Map<Course, List<Student>> enrollments;
 
+    public Enrollment() {
+        enrollments = new HashMap<>();
+    }
+
+    @Override
+    public void enrollStudent(Student student, Course course) {
+        if (!enrollments.containsKey(course)) {
+            enrollments.put(course, new ArrayList<>());
+        }
+        List<Student> students = enrollments.get(course);
+        if (course.enrollStudent(student)) {
+            students.add(student);
+            System.out.println(student.getName() + " enrolled in " + course.getName());
+        } else {
+            System.out.println("Course " + course.getName() + " is full. Enrollment failed for " + student.getName());
+        }
+    }
+
+    @Override
+    public void dropStudent(Student student, Course course) {
+        if (enrollments.containsKey(course)) {
+            List<Student> students = enrollments.get(course);
+            if (students.contains(student)) {
+                course.dropStudent(student);
+                students.remove(student);
+                System.out.println(student.getName() + " dropped from " + course.getName());
+            } else {
+                System.out.println(student.getName() + " is not enrolled in " + course.getName());
+            }
+        } else {
+            System.out.println("No enrollment record found for " + course.getName());
+        }
+    }
+
+    @Override
+    public void displayEnrollmentDetails() {
+        for (Map.Entry<Course, List<Student>> entry : enrollments.entrySet()) {
+            Course course = entry.getKey();
+            List<Student> students = entry.getValue();
+            System.out.println("Course: " + course.getName() + ", Enrolled Students: ");
+            for (Student student : students) {
+                System.out.println("- " + student.getName());
+            }
+        }
+    }
 }
