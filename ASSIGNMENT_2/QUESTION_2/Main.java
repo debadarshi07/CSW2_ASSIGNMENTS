fff137fdae976df3082a34be9eb5533a41fef341
Debadarshi Omkar <129953571+debadarshi07@users.noreply.github.com>
2024-03-12 22:04:22 +0530

    Add files via upload

---
+++ ASSIGNMENT_2/QUESTION_2/Main.java
@@ -0,0 +1,18 @@
+package Q2;
+
+public class Main {
+	@SuppressWarnings({"rawtypes", "unchecked"})
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Book book1=new Book(25,"Human Nature",500);
+		Book book2=new Book("30a","Artificial Intelligence",5000);
+		
+		System.out.println(book1);
+		System.out.println(book2);
+		
+		System.out.println(book1.equals(book2));
+//		System.out.println(book2 instanceof Book);
+	}
+
+}
