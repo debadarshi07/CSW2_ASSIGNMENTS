55a872444c324e259f709b85082c21a6893607f7
Debadarshi Omkar <129953571+debadarshi07@users.noreply.github.com>
2024-03-12 20:51:36 +0530

    Update Main.java

--- ASSIGNMENT_1/QUESTION_9/Main.java
+++ ASSIGNMENT_1/QUESTION_9/Main.java
@@ -1,8 +1,25 @@
 package Q9;
 
-public class Main {
+public class StudentMain {
+    public static void main(String[] args) {
+        Student student1 = new Student("1001", "Alice");
+        Student student2 = new Student("1002", "Bob");
 
-	public static void main(String[] args) {
-		
-	}
+        Course course1 = new Course("CSCI101", "Introduction to Computer Science", 30);
+        Course course2 = new Course("MATH101", "Calculus I", 25);
+
+        EnrollmentSystem enrollmentSystem = new Enrollment();
+        enrollmentSystem.enrollStudent(student1, course1);
+        enrollmentSystem.enrollStudent(student2, course1);
+
+        enrollmentSystem.enrollStudent(student1, course2);
+        enrollmentSystem.enrollStudent(student2, course2);
+
+        enrollmentSystem.displayEnrollmentDetails();
+
+        enrollmentSystem.dropStudent(student1, course1);
+        enrollmentSystem.dropStudent(student2, course2);
+
+        enrollmentSystem.displayEnrollmentDetails();
+    }
 }
