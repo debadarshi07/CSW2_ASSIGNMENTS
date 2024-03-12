7fe9a555c9fccdf12fc810ab61fdb1318bf34199
Debadarshi Omkar <129953571+debadarshi07@users.noreply.github.com>
2024-03-12 22:01:06 +0530

    Add files via upload

---
+++ ASSIGNMENT_2/QUESTION_1/Main.java
@@ -0,0 +1,15 @@
+package Q1;
+
+public class Main {
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	
+	public static void main(String[] args) {
+		Student s1=new Student("Debadarshi",5006,19);
+		System.out.println(s1.toString());
+		
+		Student s2=new Student("Virat Kohli","5018",35);
+		System.out.println(s2.toString());
+		
+		
+	}
+}
