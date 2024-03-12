489681b5493325565a2d0178331eb155d680eea6
Debadarshi Omkar <129953571+debadarshi07@users.noreply.github.com>
2024-03-12 19:59:46 +0530

    Add files via upload

---
+++ ASSIGNMENT_1/QUESTION_5/Main.java
@@ -0,0 +1,19 @@
+package Q5;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        College c1=new College("ITER", "Bhubaneswar");
+        College c2=new College("IIT", "Bombay");
+
+        Student s1=new Student("Debadarshi Omkar", 5006, c1);
+        Student s2=new Student("Virat Kohli", 5018, c2);
+
+        s1.displayStudentInfo();
+        System.out.println();
+        s2.displayStudentInfo();
+        
+    }
+    
+}
+
