267e8bb47acddbb051aca42d557c7624e4d6e772
Debadarshi Omkar <129953571+debadarshi07@users.noreply.github.com>
2024-03-12 20:47:28 +0530

    Update Course.java

--- ASSIGNMENT_1/QUESTION_9/Course.java
+++ ASSIGNMENT_1/QUESTION_9/Course.java
@@ -1,5 +1,44 @@
 package Q9;
 
 public class Course {
+   private String courseId;
+    private String name;
+    private int maxCapacity;
+    private List<Student> enrolledStudents;
 
+    public Course(String courseId, String name, int maxCapacity) {
+        this.courseId = courseId;
+        this.name = name;
+        this.maxCapacity = maxCapacity;
+        this.enrolledStudents = new ArrayList<>();
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public List<Student> getEnrolledStudents() {
+        return enrolledStudents;
+    }
+
+    public boolean enrollStudent(Student student) {
+        if (enrolledStudents.size() < maxCapacity) {
+            enrolledStudents.add(student);
+            return true;
+        }
+        return false;
+    }
+
+    public void dropStudent(Student student) {
+        enrolledStudents.remove(student);
+    }
+  }
 }
