d69152a3053be8c8d294bacd1fbc8048103e4a87
Debadarshi Omkar <129953571+debadarshi07@users.noreply.github.com>
2024-04-20 17:51:52 +0530

    Update MaximumOccurrence.java

--- ASSIGNMENT_3/QUESTION_10/MaximumOccurrence.java
+++ ASSIGNMENT_3/QUESTION_10/MaximumOccurrence.java
@@ -4,23 +4,15 @@ import java.util.HashMap;
 
 public class MaximumOccurrence {
     public static void findMostOccurringWords(String text) {
-        // Split the text into words
         String[] words = text.split(" ");
 
-        // Create a Hashtable to store word frequencies
         HashMap<String, Integer> wordFrequencyMap = new HashMap<>();
 
-        // Iterate through the words and update their frequencies in the Hashtable
         for (String word : words) {
-            // Remove any punctuation marks from the word
             word = word.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-
-            // Update word frequency
-            int frequency = wordFrequencyMap.getOrDefault(word, 0);
-            wordFrequencyMap.put(word, frequency + 1);
+            wordFrequencyMap.put(word, wordFrequencyMap.getOrDefault(word, 0));
         }
 
-        // Find the most occurring words
         int maxFrequency = 0;
         for (String word : wordFrequencyMap.keySet()) {
             int frequency = wordFrequencyMap.get(word);
@@ -29,7 +21,6 @@ public class MaximumOccurrence {
             }
         }
 
-        // Print the most occurring words
         System.out.println("Most occurring words in the string:");
         for (String word : wordFrequencyMap.keySet()) {
             if (wordFrequencyMap.get(word) == maxFrequency) {
@@ -39,12 +30,8 @@ public class MaximumOccurrence {
     }
 
     public static void main(String[] args) {
-        String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
-                "Integer eget massa mauris. Sed nec ullamcorper purus, " +
-                "eget laoreet felis. Sed consequat elit in feugiat ultricies. " +
-                "Praesent vel sapien at magna vestibulum mollis at in ipsum. " +
-                "Aenean in nunc id risus cursus venenatis. Pellentesque rutrum " +
-                "sapien nec neque tincidunt, at consectetur nisi tristique.";
+        
+        String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " + "Integer eget massa mauris. Sed nec ullamcorper purus, " + "eget laoreet felis. Sed consequat elit in feugiat ultricies. " + "Praesent vel sapien at magna vestibulum mollis at in ipsum. " + "Aenean in nunc id risus cursus venenatis. Pellentesque rutrum " + "sapien nec neque tincidunt, at consectetur nisi tristique.";
 
         findMostOccurringWords(text);
     }
