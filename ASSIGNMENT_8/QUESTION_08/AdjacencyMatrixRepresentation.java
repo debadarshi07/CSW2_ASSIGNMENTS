15e9e92178f2e6d91b6be38507e40c6af0ba2123
Debadarshi Omkar <129953571+debadarshi07@users.noreply.github.com>
2024-05-16 00:24:16 +0530

    Create AdjacencyMatrixRepresentation.java

---
+++ ASSIGNMENT_8/QUESTION_08/AdjacencyMatrixRepresentation.java
@@ -0,0 +1,35 @@
+package Q08;
+public class AdjacencyMatrixRepresentation {
+	int adjacencyMatrix[][];
+	int vertices;
+	AdjacencyMatrixRepresentation(int vertices){
+		this.vertices=vertices;
+		adjacencyMatrix=new int[vertices][vertices];
+	}
+	
+	void addEdge(int source,int destination) {
+		adjacencyMatrix[source][destination]=1;
+		adjacencyMatrix[destination][source]=1;
+	}
+	
+	void removeEdge(int source,int destination) {
+		adjacencyMatrix[source][destination]=0;
+		adjacencyMatrix[destination][source]=0;
+	}
+	
+	void displayAdjMatrix() {
+		for(int i=0;i<vertices;i++) {
+			for(int j=0;j<vertices;j++)
+				System.out.print(adjacencyMatrix[i][j]+" ");
+			System.out.println();
+		}
+	}
+	
+	public static void main(String[] args) {
+		AdjacencyMatrixRepresentation graph=new AdjacencyMatrixRepresentation(4);
+		graph.addEdge(0, 1);
+		graph.addEdge(1, 2);
+		graph.addEdge(1, 3);
+		graph.displayAdjMatrix();
+	}
+}
