7960ece42e9e0dc39da5be34c4489cd113617061
Debadarshi Omkar <129953571+debadarshi07@users.noreply.github.com>
2024-05-16 15:40:31 +0530

    Update Q16.java

--- ASSIGNMENT_4/Q16.java
+++ ASSIGNMENT_4/Q16.java
@@ -3,7 +3,7 @@ package ErrorHandling;
 public class Q16 {
 	public static void main(String[] args) {
 		
-		int arr[]= {1,2,3,4,5};
+		int arr[]={1,2,3,4,5};
 		
 		try {
 			int number=arr[7]+3;
@@ -12,5 +12,4 @@ public class Q16 {
 			System.out.println("You are trying to access an index which is not present.");
 		}
 	}
-
 }
