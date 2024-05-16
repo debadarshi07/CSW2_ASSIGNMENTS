3c44a99d3ba765e740487c700ea2fdc2a8a90635
Debadarshi Omkar <129953571+debadarshi07@users.noreply.github.com>
2024-05-16 13:21:47 +0530

    Update Graph.java

--- ASSIGNMENT_8/QUESTION_10/Graph.java
+++ ASSIGNMENT_8/QUESTION_10/Graph.java
@@ -1,68 +1,66 @@
 package Q10;
+import java.util.Scanner;
 import java.util.ArrayDeque;
 import java.util.Queue;
-import java.util.Scanner;
 import java.util.LinkedList;
 
 public class Graph {
-    int vertices;
-    LinkedList<Integer>[] adjList;
+	LinkedList<Integer>[] adjacentList;
+	int vertices;
 
-    public Graph(int vertices) {
-        this.vertices = vertices;
-        adjList = new LinkedList[vertices];
-        for (int i = 0; i < numVertices; i++) adjList[i] = new LinkedList<>();
-    }
+	public Graph(int vertices) {
+		this.vertices=vertices;
+		adjacentList=new LinkedList[vertices];
+		for(int i=0;i<vertices;i++) adjacentList[i]=new LinkedList<>();
+	}
 
-    public void addEdge(int source, int destination) {
-        adjList[source].add(destination);
-    }
+	public void addEdge(int source,int destination) {
+		adjacentList[source].add(destination);
+		adjacentList[destination].add(source);
+	}
 
-    public void readGraph() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number of edges: ");
-        int edges = sc.nextInt();
-        System.out.println("Enter edges (source destination):");
-        for (int i = 0; i < edges; i++) {
-            int source = sc.nextInt();
-            int destination = sc.nextInt();
-            addEdge(source, destination);
-        }
-        sc.close();
-    }
+	public void readGraph() {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the number of edges: ");
+		int edges=sc.nextInt();
+		for(int i=0;i<edges;i++) {
+			System.out.println("Enter edge "+i+":");
+			int source=sc.nextInt(),destination=sc.nextInt();
+			addEdge(source,destination);
+		}
+		sc.close();
+	}
 
-    public void bfs(int startVertex) {
-        boolean[] visited = new boolean[numVertices];
-        Queue<Integer> queue = new ArrayDeque<>();
-        visited[startVertex] = true;
-        queue.add(startVertex);
+	public void BFS(int initial) {
+		boolean[] visited=new boolean[vertices];
+		Queue<Integer> queue=new ArrayDeque<>();
+		visited[initial]=true;
+		queue.add(initial);
 
-        System.out.println("Breadth First Traversal:");
+		System.out.println("Breadth First Traversal:");
 
-        while (!queue.isEmpty()) {
-            int vertex = queue.poll();
-            System.out.print(vertex + " ");
+		while(!queue.isEmpty()) {
+			int node=queue.poll();
+			System.out.print(node + " ");
 
-            for (int neighbor : adjList[vertex]) {
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true;
-                    queue.add(neighbor);
-                }
-            }
-        }
-        System.out.println();
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number of vertices: ");
-        int vertices = sc.nextInt();
-        Graph graph = new Graph(vertices);
-        graph.readGraph();
+			for(int neighbour:adjacentList[node]) 
+				if(!visited[neighbour]) {
+					queue.add(neighbour);
+					visited[neighbour]=true;
+				}
+		}
+	}
+	public static void main(String[] args) {
 
-        System.out.print("Enter the starting vertex for BFS traversal: ");
-        int startVertex = sc.nextInt();
-        graph.bfs(startVertex);
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the number of vertices: ");
+		int vertices=sc.nextInt();
+		Graph graph=new Graph(vertices);
+		graph.readGraph();
+		System.out.print("Enter the initial vertex: ");
+		int initial=sc.nextInt();
+		graph.BFS(initial);
 
-        sc.close();
-    }
+		sc.close();
+	}
 }
