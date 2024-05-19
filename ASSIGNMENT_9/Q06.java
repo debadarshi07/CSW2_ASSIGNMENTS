0713dcddfe419657245bcc6dabb81c4a631ba250
Debadarshi Omkar <129953571+debadarshi07@users.noreply.github.com>
2024-05-19 23:13:47 +0530

    Update Q06.java

--- ASSIGNMENT_9/Q06.java
+++ ASSIGNMENT_9/Q06.java
@@ -5,13 +5,7 @@ public class Q06 {
     }
     public static void main(String[] args) {
         Function<Double, Double> square=getSquare();
-
-        double num1=15;
-        double num2=30;
-        double num3=112;
-
-        System.out.println("The square of "+num1+" is: "+square.apply(num1));
-        System.out.println("The square of "+num2+" is: "+square.apply(num2));
-        System.out.println("The square of "+num3+" is: "+square.apply(num3));
+        double num=15;
+        System.out.println("The square of "+num1+" is: "+square.apply(num));
     }
 }
