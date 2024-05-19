5b584fea10ba5f7257767e0a8944df21e601a32b
Debadarshi Omkar <129953571+debadarshi07@users.noreply.github.com>
2024-05-19 23:19:46 +0530

    Update Q11.java

--- ASSIGNMENT_9/Q11.java
+++ ASSIGNMENT_9/Q11.java
@@ -5,15 +5,12 @@ import java.time.format.DateTimeFormatter;
 public final class Q11 {
     public static void main(String[] args) {
         String utc = "2024-05-19T15:30:45Z";
-
         Instant instant=Instant.parse(utc);
 
         ZoneId targetZone=ZoneId.of("America/New_York");
-
         ZonedDateTime targetDateTime=instant.atZone(targetZone);
 
         DateTimeFormatter format=DateTimeFormatter.ofPattern("yyyy-MM-dd , HH:mm:ss");
-
         System.out.println("Formatted DateTime: " + targetDateTime.format(format));
     }
 }
