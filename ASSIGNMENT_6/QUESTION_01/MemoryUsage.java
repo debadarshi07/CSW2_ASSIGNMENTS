17fee782be9a0262ec30e0c929b34737b1844430
Debadarshi Omkar <129953571+debadarshi07@users.noreply.github.com>
2024-05-22 19:13:29 +0530

    Update MemoryUsage.java

--- ASSIGNMENT_6/QUESTION_01/MemoryUsage.java
+++ ASSIGNMENT_6/QUESTION_01/MemoryUsage.java
@@ -1,8 +1,6 @@
 
 public class MemoryUsage {
 	public static void main(String[] args) {
-		
-			// Using String literals
 			String str1 = "Java";
 			String str2 = "Java";
 			
@@ -12,8 +10,6 @@ public class MemoryUsage {
 			System.out.println("str1 == str2 : " + (str1 == str2));
 			System.out.println("str3 == str4 : " + (str3 == str4));
 			
-			System.out.println("str1.equals(str3) : " + str1.equals(str3)); 
-			
+			System.out.println("str1.equals(str3) : " + str1.equals(str3)); 	
 	}
-
 }
