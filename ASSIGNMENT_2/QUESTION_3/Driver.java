679cb3ce480bd38f8519bba076fb3a13cb927032
Debadarshi Omkar <129953571+debadarshi07@users.noreply.github.com>
2024-05-22 17:05:31 +0530

    Update Driver.java

--- ASSIGNMENT_2/QUESTION_3/Driver.java
+++ ASSIGNMENT_2/QUESTION_3/Driver.java
@@ -6,9 +6,9 @@ public class Driver {
 		Car car1=new Car("Alto","blue",200);
 		Car car2=new Car("Nano","gray",150);
 		
-		if(car1.compareTo(car2)==1) {
+		if(car1.compareTo(car2)>0) {
 			System.out.println(car1);
-		}else if(car1.compareTo(car2)==-1) {
+		}else if(car1.compareTo(car2)<0) {
 			System.out.println(car2);
 		}else {
 			System.out.println("Both the cars have the same speed.");
